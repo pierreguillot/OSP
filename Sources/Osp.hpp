c061d08bc04983ff49894ecd5b9f6be6f1024a2c
pierreguillot <guillotpierre6@gmail.com>
2015-06-01 21:45:58 +0200

    Some new methods

--- Sources/Osp.hpp
+++ Sources/Osp.hpp
@@ -28,22 +28,144 @@ namespace osp
             memset(in, 0, size * sizeof(T));
         }
         
-        //! Fills a vector a vector with a value.
-        /** Fills a vector a vector with a value
+        //! Fills a vector with a value.
+        /** Fills a vector with a value
          @param size  The size of the vector.
          @param value The value.
          @param in    The input vector.
          */
         static inline void fill(size_t const size, T const& value, T* in) noexcept
         {
+            const T vec[8] = {value, value, value, value, value, value, value, value};
             for(size_t i = size>>3; i; --i, in += 8)
             {
-                in[0] = value; in[1] = value; in[2] = value; in[3] = value;
-                in[4] = value; in[5] = value; in[6] = value; in[7] = value;
+                memcpy(in, vec, 8 * sizeof(T));
+            }
+            if(size&7)
+            {
+                memcpy(in, vec, (size&7) * sizeof(T));
+            }
+        }
+        
+        //! Swaps two vectors.
+        /** Swaps two vectors.
+         @param size The size of the vectors.
+         @param in1  The first vector.
+         @param in2  The second vector.
+         */
+        static inline void swap(const size_t size, T* in1, T* in2) noexcept
+        {
+            T vec[8];
+            for(size_t i = size>>3; i; --i, in1 += 8, in2 += 8)
+            {
+                memcpy(vec, in1, 8 * sizeof(T));
+                memcpy(in1, in2, 8 * sizeof(T));
+                memcpy(in2, vec, 8 * sizeof(T));
+            }
+            if(size&7)
+            {
+                memcpy(vec, in1, (size&7) * sizeof(T));
+                memcpy(in1, in2, (size&7) * sizeof(T));
+                memcpy(in2, vec, (size&7) * sizeof(T));
+            }
+        }
+        
+        //! Copies a vector into another.
+        /** Copies a vector into another.
+         @param size The size of the vectors.
+         @param in   The input vector.
+         @param out  The output vector.
+         */
+        static inline void copy(const size_t size, const T* in, T* out) noexcept
+        {
+            for(size_t i = size>>3; i; --i, in += 8, out += 8)
+            {
+                memcpy(out, in, 8 * sizeof(T));
+            }
+            if(size&7)
+            {
+                memcpy(out, in, (size&7) * sizeof(T));
+            }
+        }
+        
+        //! Adds a vector to another.
+        /** Adds a vector to another.
+         @param size The size of the vectors.
+         @param in   The input vector.
+         @param out  The output vector.
+         */
+        static inline void add(const size_t size, const T* in, T* out) noexcept
+        {
+            for(size_t i = size>>3; i; --i, in += 8, out += 8)
+            {
+                out[0] += in[0]; out[1] += in[1]; out[2] += in[2]; out[3] += in[3];
+                out[4] += in[4]; out[5] += in[5]; out[6] += in[6]; out[7] += in[7];
+            }
+            for(size_t i = size&7; i; --i, in++, out++)
+            {
+                out[0] += in[0];
+            }
+        }
+        
+        //! Adds a two vectors and leaves the result in another.
+        /** Adds a two vectors and leaves the result in another.
+         @param size The size of the vectors.
+         @param in1  The first input vector.
+         @param in2  The second input vector.
+         @param out  The output vector.
+         */
+        static inline void add(const size_t size, const T* in1, const T* in2, T* out) noexcept
+        {
+            for(size_t i = size>>3; i; --i, in1 += 8, in2 += 8, out += 8)
+            {
+                const T f0 = in1[0], f1 = in1[1], f2 = in1[2], f3 = in1[3];
+                const T f4 = in1[4], f5 = in1[5], f6 = in1[6], f7 = in1[7];
+                out[0] = f0 + in2[0]; out[1] = f1 + in2[1]; out[2] = f2 + in2[2]; out[3] = f3 + in2[3];
+                out[4] = f4 + in2[4]; out[5] = f5 + in2[5]; out[6] = f6 + in2[6]; out[7] = f7 + in2[7];
+            }
+            for(size_t i = size&7; i; --i, in1++, in2++, out++)
+            {
+                out[0] += in1[0] + in2[0];
+            }
+        }
+        
+        //! Adds a scalar to a vector.
+        /** Adds a scalar to a vector.
+         @param size  The size of the vectors.
+         @param value The value.
+         @param in    The vector.
+         */
+        static inline void add(const size_t size, T const& value, T* in) noexcept
+        {
+            for(size_t i = size>>3; i; --i, in += 8)
+            {
+                in[0] += value; in[1] += value; in[2] += value; in[3] += value;
+                in[4] += value; in[5] += value; in[6] += value; in[7] += value;
             }
             for(size_t i = size&7; i; --i, in++)
             {
-                in[0] = value;
+                in[0] += value;
+            }
+        }
+        
+        //! Adds a scalar to a vector and leaves the result in another.
+        /** Adds a scalar to a vector and leaves the result in another.
+         @param size  The size of the vectors.
+         @param value The value.
+         @param in    The vector.
+         @param out   The output vector.
+         */
+        static inline void add(const size_t size, T const& value, const T* in, T* out) noexcept
+        {
+            for(size_t i = size>>3; i; --i, in += 8, out += 8)
+            {
+                out[0] = value + in[0]; out[1] = value + in[1]; out[2] = value + in[2]; out[3] = value + in[3];
+                out[4] = value + in[4]; out[5] = value + in[5]; out[6] = value + in[6]; out[7] = value + in[7];
+                
+            }
+            for(size_t i = size&7; i; --i, in++, out++)
+            {
+                out[0] += value + in[0];
             }
         }
         
--- Tests/test1/test1/main.cpp
+++ Tests/test1/test1/main.cpp
@@ -47,6 +47,7 @@ inline void stop_clock()
 
 inline void post(int size , const float *in)
 {
+    std::cout << "\n";
     for(int i = 0; i < size; i++)
     {
         std::cout << in[i] << " ";
@@ -56,8 +57,9 @@ inline void post(int size , const float *in)
 
 int main(int argc, const char * argv[])
 {
+    //size_t size = 1024;
     //size_t size = 64 +7;
-    size_t size =16*1024*1024;
+    size_t size =16*1024*1024-1;
     size_t nloop = size>>3;
     size_t remain = size&7;
     std::cout << "Size : "<< size << " Nloops : " << (size>>2)  << " Remain : " << (size&3) << " " << 2097151 *2 <<"\n";
@@ -65,33 +67,60 @@ int main(int argc, const char * argv[])
     
     float* in1 = new (std::nothrow) float[size];
     float* in2 = new (std::nothrow) float[size];
+    float* out = new (std::nothrow) float[size];
     if(!in1 || !in2)
     {
         std::cout << "alloc error" << "\n";
         return 0;
     }
+    Osp<float>::fill(size, 1.f, in1);
+    std::cout << "\nClear : \n";
+    start_clock();
+    Osp<float>::clear(size, in1);
+    stop_clock();
+    
     std::cout << "\nFill : \n";
     start_clock();
     Osp<float>::fill(size, 1.f, in1);
     stop_clock();
-    Osp<float>::fill(size, 1.f, in2);
+    Osp<float>::fill(size, 2.f, in2);
     
     std::cout << "\nDot : \n";
     start_clock();
-    float result = 0;
-        result = Osp<float>::dot(size, in1, in2);
+    Osp<float>::dot(size, in1, in2);
     stop_clock();
+    
+    std::cout << "\nSwap : \n";
     start_clock();
-    float result2 = 0;
-        result2 = cblas_sdot(int(size), in1, 1, in2, 1);
+    Osp<float>::swap(size, in1, in2);
     stop_clock();
     
-    if(result != result2)
-        std::cout << "false : "<< result << " " << result2 << "\n";
-    else
-        std::cout << "true : "<< result << "\n";
+    std::cout << "\nCopy : \n";
+    start_clock();
+    Osp<float>::copy(size, in1, in2);
+    stop_clock();
     
+    std::cout << "\nAdd vector : \n";
+    start_clock();
+    Osp<float>::add(size, in1, in2);
+    stop_clock();
     
+    std::cout << "\nAdd scalar : \n";
+    start_clock();
+    Osp<float>::add(size, 1.4f, in1);
+    stop_clock();
+    
+    std::cout << "\nAdd vector2 : \n";
+    start_clock();
+    Osp<float>::add(size, in1, in2, out);
+    stop_clock();
+    
+    std::cout << "\nAdd scalar2 : \n";
+    start_clock();
+    Osp<float>::add(size, 2.6, in1, out);
+    stop_clock();
+    
+    post(10, out);
     
     delete [] in1;
     delete [] in2;
